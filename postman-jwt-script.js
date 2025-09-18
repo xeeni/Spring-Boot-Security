5ab1d8f92a85d60cb53a2888a5f627c4961aab3c
zeena <zeenattanver@gmail.com>
2025-09-19 00:46:29 +0100

    Revert "Spring security project"
    This reverts commit 8443ce389f4d46b5a37a69f22f834dcda694c02d.

--- postman-jwt-script.js
+++
@@ -1,7 +0,0 @@
-// Add this script to your Login request → Tests tab in Postman
-
-if (pm.response.code === 200) {
-    var jsonData = pm.response.json();
-    pm.environment.set("jwtToken", jsonData.token);
-    console.log("JWT Token saved:", jsonData.token);
-}
